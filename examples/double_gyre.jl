6975764dbc1b62337c19954e2de752812ff108ab
Gregory Wagner <wagner.greg@gmail.com>
2020-10-25 22:27:08 -0400

    Minor beautifications

--- examples/double_gyre.jl
+++ examples/double_gyre.jl
@@ -1,90 +1,95 @@
 # # Double Gyre
 #
-# This example simulates a double gyre following: https://mitgcm.readthedocs.io/en/latest/examples/baroclinic_gyre/baroclinic_gyre.html
+# This example simulates a double gyre following:
+# https://mitgcm.readthedocs.io/en/latest/examples/baroclinic_gyre/baroclinic_gyre.html
 
+using Oceananigans
 using Oceananigans.Grids
+using Oceananigans.Utils
+using Oceananigans.BoundaryConditions
+using Oceananigans.Advection
+using Oceananigans.Diagnostics
 
-grid = RegularCartesianGrid(size = (128, 128, 32),
-                               x = (-2.5e6, 2.5e6),
-                               y = (-2.5e6, 2.5e6),
-                               z = (-1.8e3, 0),
-                            halo = (3, 3, 3),
-                        topology = (Bounded, Bounded, Bounded))
+using Printf
+
+grid = RegularCartesianGrid(size=(48, 48, 16), x=(-2e6, 2e6), y=(-3e6, 3e6), z=(-2e3, 0),
+                            topology=(Bounded, Bounded, Bounded))
 
 # ## Boundary conditions
 
-using Oceananigans.BoundaryConditions
+@inline wind_stress(x, y, t, parameters) = - parameters.τ * cos(2π * y / parameters.L)
 
-@inline wind_stress(x, y, t, parameters) = - parameters.τ * cos(2π * y / parameters.Ly)
+@inline u_bottom_stress(i, j, grid, clock, model_fields, parameters) =
+    @inbounds - parameters.μ * parameters.H * model_fields.u[i, j, 1]
+
+@inline v_bottom_stress(i, j, grid, clock, model_fields, parameters) =
+    @inbounds - parameters.μ * parameters.H * model_fields.v[i, j, 1]
+
+wind_stress_bc = BoundaryCondition(Flux, wind_stress, parameters = (τ=1e-4, L=grid.Ly))
+
+u_bottom_stress_bc = BoundaryCondition(Flux, u_bottom_stress,
+                                       discrete_form=true, parameters=(μ=1/30day, H=grid.Lz))
+
+v_bottom_stress_bc = BoundaryCondition(Flux, v_bottom_stress,
+                                       discrete_form=true, parameters=(μ=1/30day, H=grid.Lz))
 
 u_bcs = UVelocityBoundaryConditions(grid,
-              top = BoundaryCondition(Flux, wind_stress, parameters = (τ = 1e-4, Ly = grid.Ly)))
+                                     north = BoundaryCondition(Value, 0),
+                                     south = BoundaryCondition(Value, 0),
+                                       top = wind_stress_bc,
+                                    bottom = u_bottom_stress_bc)
+
+v_bcs = VVelocityBoundaryConditions(grid,
+                                      east = BoundaryCondition(Value, 0),
+                                      west = BoundaryCondition(Value, 0),
+                                    bottom = v_bottom_stress_bc)
+
+w_bcs = WVelocityBoundaryConditions(grid,
+                                    north = BoundaryCondition(Value, 0),
+                                    south = BoundaryCondition(Value, 0),
+                                     east = BoundaryCondition(Value, 0),
+                                     west = BoundaryCondition(Value, 0))
 
 b_reference(y, parameters) = parameters.Δb / parameters.Ly * y
 
-using Oceananigans.Utils
+@inline buoyancy_flux(i, j, grid, clock, model_fields, parameters) =
+    @inbounds - parameters.μ * (model_fields.b[i, j, grid.Nz] - b_reference(grid.yC[j], parameters))
 
-@inline buoyancy_flux(i, j, grid, clock, model_fields, parameters) = @inbounds - parameters.μ * (model_fields.b[i, j, grid.Nz] - b_reference(grid.yC[j], parameters))
+buoyancy_flux_bc = BoundaryCondition(Flux, buoyancy_flux,
+                                     discrete_form = true,
+                                     parameters = (μ=1/day, Δb=0.06, Ly=grid.Ly))
+
 b_bcs = TracerBoundaryConditions(grid, 
-              top = BoundaryCondition(Flux, buoyancy_flux, discrete_form = true, parameters = (μ = 50 / 30day, Δb = 0.055, Ly = grid.Ly)))
+                                 bottom = BoundaryCondition(Value, 0),
+                                 top = buoyancy_flux_bc)
 
-using Oceananigans, Oceananigans.TurbulenceClosures, Oceananigans.Advection
+closure = AnisotropicDiffusivity(νh=500, νz=1e-2, κh=100, κz=1e-2)
 
-closure = AnisotropicDiffusivity(νh = 5e3, νz = 1e-2, κh = 500, κz = 1e-2)
-
-# closure = (AnisotropicDiffusivity(νh = 5e3, νz = 1e-2, κh = 500, κz = 1e-2),
-           # AnisotropicBiharmonicDiffusivity(νh = 1e-3*grid.Δx^4/day, νz = 0, κh = 1e-3*grid.Δx^4/day, κz = 0))
-
-model = IncompressibleModel(       architecture = CPU(),
-                                    timestepper = :RungeKutta3, 
-                                      advection = CenteredFourthOrder(),
-                                           grid = grid,
-                                       coriolis = BetaPlane(latitude = 45),
-                                       buoyancy = BuoyancyTracer(),
-                                        tracers = :b,
-                                        closure = closure,
-                            boundary_conditions = (u=u_bcs, b=b_bcs))
-nothing # hide
+model = IncompressibleModel(architecture = CPU(),
+                            timestepper = :RungeKutta3, 
+                            advection = UpwindBiasedFifthOrder(),
+                            grid = grid,
+                            coriolis = BetaPlane(latitude=45),
+                            buoyancy = BuoyancyTracer(),
+                            tracers = :b,
+                            closure = closure,
+                            boundary_conditions = (u=u_bcs, v=v_bcs, w=w_bcs, b=b_bcs))
 
 ## Temperature initial condition: a stable density gradient with random noise superposed.
 b₀(x, y, z) = b_bcs.top.condition.parameters.Δb * (1 + z / grid.Lz)
 
 set!(model, b=b₀)
 
-# ## Set up output
-#
-# We set up an output writer that saves all velocity fields, tracer fields, and the subgrid
-# turbulent diffusivity associated with `model.closure`. The `prefix` keyword argument
-# to `JLD2OutputWriter` indicates that output will be saved in
-# `double_gyre.jld2`.
-
-using Oceananigans.OutputWriters
-
-## Instantiate a JLD2OutputWriter to write fields. We will add it to the simulation before
-## running it.
-field_writer = JLD2OutputWriter(model, merge(model.velocities, model.tracers);
-                                time_interval=1day,
-                                prefix="double_gyre",
-                                field_slicer=FieldSlicer(k=model.grid.Nz),
-                                force=true)
-                                                                 
 # ## Running the simulation
 #
 # To run the simulation, we instantiate a `TimeStepWizard` to ensure stable time-stepping
 # with a Courant-Freidrichs-Lewy (CFL) number of 0.2.
 
-wizard = TimeStepWizard(cfl = 0.50,
-                         Δt = 120minute,
-                 max_change = 1.1,
-                     max_Δt = minimum([0.1*grid.Δz^2/closure.κz, 
-                                       0.1*grid.Δx^2/closure.νx]))
-                     # max_Δt = minimum([0.1*grid.Δz^2/closure[1].κz, 
-                     #                   0.1*grid.Δx^4/closure[2].κx]))
-nothing # hide
+max_Δt = min(hour, 0.5 * min(grid.Δz^2 / closure.κz, grid.Δx^2 / closure.νx))
+
+wizard = TimeStepWizard(cfl=0.5, Δt=hour/2, max_change=1.1, max_Δt=max_Δt)
 
 # Finally, we set up and run the the simulation.
-
-using Oceananigans.Diagnostics, Printf
 
 umax = FieldMaximum(abs, model.velocities.u)
 vmax = FieldMaximum(abs, model.velocities.v)
@@ -109,11 +114,28 @@ function print_progress(simulation)
     return nothing
 end
 
-simulation = Simulation(model, Δt=wizard, stop_time=10*365day, iteration_interval=200, progress=print_progress)
-simulation.output_writers[:fields] = field_writer
+simulation = Simulation(model, Δt=wizard, stop_time=20*365day, iteration_interval=100, progress=print_progress)
+
+# ## Set up output
+#
+# We set up an output writer that saves all velocity fields, tracer fields, and the subgrid
+# turbulent diffusivity associated with `model.closure`. The `prefix` keyword argument
+# to `JLD2OutputWriter` indicates that output will be saved in
+# `double_gyre.jld2`.
+
+using Oceananigans.OutputWriters
+
+#=
+## Instantiate a JLD2OutputWriter to write fields. We will add it to the simulation before
+## running it.
+simulation.output_writers[:fields] = JLD2OutputWriter(model, merge(model.velocities, model.tracers);
+                                                      time_interval=2day,
+                                                      prefix="double_gyre",
+                                                      field_slicer=FieldSlicer(k=model.grid.Nz),
+                                                      force=true)
 
 run!(simulation)
-
+=#
 
 # # Making a neat movie
 #
@@ -123,32 +145,34 @@ run!(simulation)
 # Making the coordinate arrays takes a few lines of code,
 
 x, y, z = nodes(model.tracers.b)
-x, y, z = x[:], y[:], z[:]
 nothing # hide
 
 # Next, we open the JLD2 file, and extract the iterations we ended up saving at,
 
 using JLD2, Plots
 
-file = jldopen(simulation.output_writers[:fields].filepath)
+file = jldopen("double_gyre.jld2")
 
 iterations = parse.(Int, keys(file["timeseries/t"]))
 nothing # hide
 
-# This utility is handy for calculating nice contour intervals:
+# These utilities are handy for calculating nice contour intervals:
 
-function nice_divergent_levels(c, clim)
-    levels = range(-clim, stop=clim, length=20)
-
+""" Returns colorbar levels equispaced from `(-clim, clim)` and encompassing the extrema of `c`. """
+function divergent_levels(c, clim, nlevels=21)
+    levels = range(-clim, stop=clim, length=nlevels)
     cmax = maximum(abs, c)
-
-    if clim < cmax # add levels on either end
-        levels = vcat([-cmax], range(-clim, stop=clim, length=10), [cmax])
-    end
-
-    return levels
+    return ((-clim, clim), clim > cmax ? levels : levels = vcat([-cmax], levels, [cmax]))
 end
-nothing # hide
+
+""" Returns colorbar levels equispaced between `clims` and encompassing the extrema of `c`."""
+function sequential_levels(c, clims, nlevels=20)
+    levels = range(clims[1], stop=clims[2], length=nlevels)
+    cmin, cmax = minimum(c), maximum(c)
+    cmin < clims[1] && (levels = vcat([cmin], levels))
+    cmax > clims[2] && (levels = vcat(levels, [cmax]))
+    return clims, levels
+end
 
 # Finally, we're ready to animate.
 
@@ -171,36 +195,42 @@ anim = @animate for (i, iter) in enumerate(iterations)
     
     speed = @. sqrt(uxy^2 + vxy^2)
     
-    ulim = 1.0
-    ulevels = nice_divergent_levels(u, ulim)
+    ulims, ulevels = divergent_levels(u, ulim)
+    slims, slevels = sequential_levels(speed, (0.0, 1.0))
 
-    uxy_plot = heatmap(x / 1e3, y / 1e3, uxy';
+    xlims = (-grid.Lx/2 * 1e-3, grid.Lx/2 * 1e-3)
+    ylims = (-grid.Ly/2 * 1e-3, grid.Ly/2 * 1e-3)
+
+    uxy_plot = contourf(x / 1e3, y / 1e3, uxy';
+                          linewidth = 0,
                               color = :balance,
                         aspectratio = :equal,
-                              # clims = (-2, 2),
-                             # levels = ulevels,
-                              xlims = (-grid.Lx/2e3, grid.Lx/2e3),
-                              ylims = (-grid.Ly/2e3, grid.Ly/2e3),
+                              clims = ulims,
+                             levels = ulevels,
+                              xlims = xlims,
+                              ylims = ylims,
                              xlabel = "x (km)",
                              ylabel = "y (km)")
                         
-     wxy_plot = heatmap(x / 1e3, y / 1e3, wxy';
+     wxy_plot = contourf(x / 1e3, y / 1e3, wxy';
+                           linewidth = 0,
                                color = :balance,
                          aspectratio = :equal,
-                               # clims = (-1e-2, 1e-2),
-                              # levels = ulevels,
-                               xlims = (-grid.Lx/2e3, grid.Lx/2e3),
-                               ylims = (-grid.Ly/2e3, grid.Ly/2e3),
+                               clims = ulims,
+                              levels = ulevels,
+                               xlims = xlims,
+                               ylims = ylims,
                               xlabel = "x (km)",
                               ylabel = "y (km)")
                          
-    speed_plot = heatmap(x / 1e3, y / 1e3 , speed';
-                              color = :deep,
+    speed_plot = contourf(x / 1e3, y / 1e3 , speed';
+                          linewidth = 0,
+                              color = :thermal,
                         aspectratio = :equal,
-                              clims = (0, 2.0),
-                             # levels = ulevels,
-                              xlims = (-grid.Lx/2e3, grid.Lx/2e3),
-                              ylims = (-grid.Ly/2e3, grid.Ly/2e3),
+                              clims = slims,
+                             levels = slevels,
+                              xlims = xlims,
+                              ylims = ylims,
                              xlabel = "x (km)",
                              ylabel = "y (km)")
                              
