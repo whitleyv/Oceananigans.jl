0d87464d708385c5f7682e54d06913fdf9c10111
Gregory Wagner <wagner.greg@gmail.com>
2020-10-25 23:23:18 -0400

    Fiddling with double gyre

--- examples/double_gyre.jl
+++ examples/double_gyre.jl
@@ -4,64 +4,41 @@
 # https://mitgcm.readthedocs.io/en/latest/examples/baroclinic_gyre/baroclinic_gyre.html
 
 using Oceananigans
-using Oceananigans.Grids
 using Oceananigans.Utils
+using Oceananigans.Grids
+using Oceananigans.Fields
 using Oceananigans.BoundaryConditions
 using Oceananigans.Advection
 using Oceananigans.Diagnostics
+using Oceananigans.OutputWriters
+using Oceananigans.AbstractOperations
 
 using Printf
 
-grid = RegularCartesianGrid(size=(48, 48, 16), x=(-2e6, 2e6), y=(-3e6, 3e6), z=(-2e3, 0),
+grid = RegularCartesianGrid(size=(32, 32, 8), x=(-2e5, 2e5), y=(-3e5, 3e5), z=(-1e3, 0),
                             topology=(Bounded, Bounded, Bounded))
 
 # ## Boundary conditions
 
-@inline wind_stress(x, y, t, parameters) = - parameters.τ * cos(2π * y / parameters.L)
+@inline wind_stress(x, y, t, p) = - p.τ * cos(2π * y / p.Ly)
+@inline bottom_drag_u(x, y, t, u, p) = - p.μ * p.Lz * u
+@inline bottom_drag_v(x, y, t, v, p) = - p.μ * p.Lz * v
 
-@inline u_bottom_stress(i, j, grid, clock, model_fields, parameters) =
-    @inbounds - parameters.μ * parameters.H * model_fields.u[i, j, 1]
+surface_stress_u_bc = BoundaryCondition(Flux, wind_stress, parameters=(τ=1e-3, Ly=grid.Ly))
+bottom_drag_u_bc = BoundaryCondition(Flux, bottom_drag_u, field_dependencies=:u, parameters=(μ=1/day, Lz=grid.Lz))
+bottom_drag_v_bc = BoundaryCondition(Flux, bottom_drag_v, field_dependencies=:v, parameters=(μ=1/day, Lz=grid.Lz))
 
-@inline v_bottom_stress(i, j, grid, clock, model_fields, parameters) =
-    @inbounds - parameters.μ * parameters.H * model_fields.v[i, j, 1]
+u_bcs = UVelocityBoundaryConditions(grid, top = surface_stress_u_bc, bottom = bottom_drag_u_bc)
+v_bcs = VVelocityBoundaryConditions(grid, bottom = bottom_drag_v_bc)
 
-wind_stress_bc = BoundaryCondition(Flux, wind_stress, parameters = (τ=1e-4, L=grid.Ly))
-
-u_bottom_stress_bc = BoundaryCondition(Flux, u_bottom_stress,
-                                       discrete_form=true, parameters=(μ=1/30day, H=grid.Lz))
-
-v_bottom_stress_bc = BoundaryCondition(Flux, v_bottom_stress,
-                                       discrete_form=true, parameters=(μ=1/30day, H=grid.Lz))
-
-u_bcs = UVelocityBoundaryConditions(grid,
-                                     north = BoundaryCondition(Value, 0),
-                                     south = BoundaryCondition(Value, 0),
-                                       top = wind_stress_bc,
-                                    bottom = u_bottom_stress_bc)
-
-v_bcs = VVelocityBoundaryConditions(grid,
-                                      east = BoundaryCondition(Value, 0),
-                                      west = BoundaryCondition(Value, 0),
-                                    bottom = v_bottom_stress_bc)
-
-w_bcs = WVelocityBoundaryConditions(grid,
-                                    north = BoundaryCondition(Value, 0),
-                                    south = BoundaryCondition(Value, 0),
-                                     east = BoundaryCondition(Value, 0),
-                                     west = BoundaryCondition(Value, 0))
-
-b_reference(y, parameters) = parameters.Δb / parameters.Ly * y
-
-@inline buoyancy_flux(i, j, grid, clock, model_fields, parameters) =
-    @inbounds - parameters.μ * (model_fields.b[i, j, grid.Nz] - b_reference(grid.yC[j], parameters))
+@inline buoyancy_flux(x, y, t, b, p) = - p.μ * (b - p.Δb / p.Ly * y)
 
 buoyancy_flux_bc = BoundaryCondition(Flux, buoyancy_flux,
-                                     discrete_form = true,
-                                     parameters = (μ=1/day, Δb=0.06, Ly=grid.Ly))
+                                     field_dependencies = :b,
+                                     parameters = (μ=1/day, Δb=0.1, Ly=grid.Ly))
 
-b_bcs = TracerBoundaryConditions(grid, 
-                                 bottom = BoundaryCondition(Value, 0),
-                                 top = buoyancy_flux_bc)
+b_bcs = TracerBoundaryConditions(grid, top = buoyancy_flux_bc,
+                                       bottom = BoundaryCondition(Value, 0))
 
 closure = AnisotropicDiffusivity(νh=500, νz=1e-2, κh=100, κz=1e-2)
 
@@ -73,9 +50,9 @@ model = IncompressibleModel(architecture = CPU(),
                             buoyancy = BuoyancyTracer(),
                             tracers = :b,
                             closure = closure,
-                            boundary_conditions = (u=u_bcs, v=v_bcs, w=w_bcs, b=b_bcs))
+                            boundary_conditions = (u=u_bcs, v=v_bcs, b=b_bcs))
 
-## Temperature initial condition: a stable density gradient with random noise superposed.
+## Temperature initial condition: a stable density gradient
 b₀(x, y, z) = b_bcs.top.condition.parameters.Δb * (1 + z / grid.Lz)
 
 set!(model, b=b₀)
@@ -83,11 +60,11 @@ set!(model, b=b₀)
 # ## Running the simulation
 #
 # To run the simulation, we instantiate a `TimeStepWizard` to ensure stable time-stepping
-# with a Courant-Freidrichs-Lewy (CFL) number of 0.2.
+# with a Courant-Freidrichs-Lewy (CFL) number of 1.0.
 
-max_Δt = min(hour, 0.5 * min(grid.Δz^2 / closure.κz, grid.Δx^2 / closure.νx))
+max_Δt = min(1/3model.coriolis.f₀, min(grid.Δz^2 / closure.κz, grid.Δx^2 / closure.νx))
 
-wizard = TimeStepWizard(cfl=0.5, Δt=hour/2, max_change=1.1, max_Δt=max_Δt)
+wizard = TimeStepWizard(cfl=1.0, Δt=max_Δt/2, max_change=1.1, max_Δt=max_Δt)
 
 # Finally, we set up and run the the simulation.
 
@@ -114,37 +91,47 @@ function print_progress(simulation)
     return nothing
 end
 
-simulation = Simulation(model, Δt=wizard, stop_time=20*365day, iteration_interval=100, progress=print_progress)
+simulation = Simulation(model, Δt=wizard, stop_time=30day, iteration_interval=10, progress=print_progress)
 
 # ## Set up output
 #
 # We set up an output writer that saves all velocity fields, tracer fields, and the subgrid
 # turbulent diffusivity associated with `model.closure`. The `prefix` keyword argument
-# to `JLD2OutputWriter` indicates that output will be saved in
-# `double_gyre.jld2`.
+# to `JLD2OutputWriter` indicates that output will be saved in `double_gyre.jld2`.
 
-using Oceananigans.OutputWriters
+u, v, w = model.velocities
 
-#=
-## Instantiate a JLD2OutputWriter to write fields. We will add it to the simulation before
-## running it.
-simulation.output_writers[:fields] = JLD2OutputWriter(model, merge(model.velocities, model.tracers);
-                                                      time_interval=2day,
-                                                      prefix="double_gyre",
-                                                      field_slicer=FieldSlicer(k=model.grid.Nz),
-                                                      force=true)
+speed = ComputedField(u^2 + v^2)
+
+outputs = merge(model.velocities, model.tracers, (speed=speed,))
+
+simulation.output_writers[:fields] = JLD2OutputWriter(model, outputs,
+                                                      schedule = TimeInterval(5day),
+                                                      prefix = "double_gyre",
+                                                      field_slicer = FieldSlicer(k=model.grid.Nz),
+                                                      force = true)
+
+barotropic_u = AveragedField(model.velocities.u, dims=3)
+barotropic_v = AveragedField(model.velocities.v, dims=3)
+
+simulation.output_writers[:barotropic_velocities] =
+    JLD2OutputWriter(model, (u=barotropic_u, v=barotropic_v),
+                     schedule = AveragedTimeInterval(30day, window=10day),
+                     prefix = "double_gyre_circulation",
+                     force = true)
 
 run!(simulation)
-=#
 
-# # Making a neat movie
-#
-# We look at the results by plotting vertical slices of $u$ and $w$, and a horizontal
-# slice of $w$ to look for Langmuir cells.
+# # A neat movie
 
-# Making the coordinate arrays takes a few lines of code,
+x, y, z = nodes(model.velocities.u)
 
-x, y, z = nodes(model.tracers.b)
+xlims = (-grid.Lx/2 * 1e-3, grid.Lx/2 * 1e-3)
+ylims = (-grid.Ly/2 * 1e-3, grid.Ly/2 * 1e-3)
+
+x_km = x * 1e-3
+y_km = y * 1e-3
+
 nothing # hide
 
 # Next, we open the JLD2 file, and extract the iterations we ended up saving at,
@@ -154,7 +141,6 @@ using JLD2, Plots
 file = jldopen("double_gyre.jld2")
 
 iterations = parse.(Int, keys(file["timeseries/t"]))
-nothing # hide
 
 # These utilities are handy for calculating nice contour intervals:
 
@@ -182,61 +168,57 @@ anim = @animate for (i, iter) in enumerate(iterations)
     
     @info "Drawing frame $i from iteration $iter \n"
 
-    ## Load 3D fields from file, omitting halo regions
-    u = file["timeseries/u/$iter"]
-    v = file["timeseries/v/$iter"]
-    w = file["timeseries/w/$iter"]
     t = file["timeseries/t/$iter"]
+    u = file["timeseries/u/$iter"][:, :, 1]
+    v = file["timeseries/v/$iter"][:, :, 1]
+    s = file["timeseries/speed/$iter"][:, :, 1]
 
-    ## Extract slices
-    uxy = 1/2 * (u[1:end-1, :, end] .+ u[2:end, :, end])
-    vxy = 1/2 * (v[:, 1:end-1, end] .+ v[:, 2:end, end])
-    wxy = w[:, :, 1]
-    
-    speed = @. sqrt(uxy^2 + vxy^2)
-    
-    ulims, ulevels = divergent_levels(u, ulim)
-    slims, slevels = sequential_levels(speed, (0.0, 1.0))
+    ulims, ulevels = divergent_levels(u, 1.0)
+    slims, slevels = sequential_levels(s, (0.0, 1.5))
 
-    xlims = (-grid.Lx/2 * 1e-3, grid.Lx/2 * 1e-3)
-    ylims = (-grid.Ly/2 * 1e-3, grid.Ly/2 * 1e-3)
+    kwargs = (aspectratio=:equal, linewidth=0, xlims=xlims,
+              ylims=ylims, xlabel="x (km)", ylabel="y (km)")
 
-    uxy_plot = contourf(x / 1e3, y / 1e3, uxy';
-                          linewidth = 0,
-                              color = :balance,
-                        aspectratio = :equal,
-                              clims = ulims,
-                             levels = ulevels,
-                              xlims = xlims,
-                              ylims = ylims,
-                             xlabel = "x (km)",
-                             ylabel = "y (km)")
+    u_plot = contourf(x_km, y_km, u';
+                      color = :balance,
+                      clims = ulims,
+                      levels = ulevels,
+                      kwargs...)
                         
-     wxy_plot = contourf(x / 1e3, y / 1e3, wxy';
-                           linewidth = 0,
-                               color = :balance,
-                         aspectratio = :equal,
-                               clims = ulims,
-                              levels = ulevels,
-                               xlims = xlims,
-                               ylims = ylims,
-                              xlabel = "x (km)",
-                              ylabel = "y (km)")
-                         
-    speed_plot = contourf(x / 1e3, y / 1e3 , speed';
-                          linewidth = 0,
-                              color = :thermal,
-                        aspectratio = :equal,
-                              clims = slims,
-                             levels = slevels,
-                              xlims = xlims,
-                              ylims = ylims,
-                             xlabel = "x (km)",
-                             ylabel = "y (km)")
+    s_plot = contourf(x_km, y_km, s';
+                      color = :thermal,
+                      clims = slims,
+                      levels = slevels,
+                      kwargs...)
                              
-    plot(uxy_plot, speed_plot, size=(1100, 500), title = ["u(t="*string(round(t/day, digits=1))*" day)" "speed"])
+    plot(u_plot, s_plot, size=(800, 500),
+         title = ["u(t="*string(round(t/day, digits=1))*" day)" "speed"])
 
     iter == iterations[end] && close(file)
 end
 
-gif(anim, "double_gyre.gif", fps = 12) # hide
+gif(anim, "double_gyre.gif", fps = 8) # hide
+
+# Plot the barotropic circulation
+
+xv, yv, zv = nodes(model.velocities.v)
+
+xv_km, yv_km = xv * 1e-3, yv * 1e-3
+
+file = jldopen("double_gyre_circulation.jld2")
+
+last_iteration = parse(Int, last(keys(file["timeseries/t"])))
+
+U = file["timeseries/u/$last_iteration"][:, :, 1]
+V = file["timeseries/v/$last_iteration"][:, :, 1]
+
+U_plot = contourf(x_km, y_km, U', xlims=xlims, ylims=ylims,
+                  linewidth=0, color=:balance, aspectratio=:equal)
+
+V_plot = contourf(xv_km, yv_km, V', xlims=xlims, ylims=ylims,
+                  linewidth=0, color=:balance, aspectratio=:equal)
+
+plot(U_plot, V_plot, size=(800, 500),
+     title=["Depth- and time-averaged \$ u \$" "Depth- and time-averaged \$ v \$"])
+
+savefig("double_gyre_circulation.png") # hide
