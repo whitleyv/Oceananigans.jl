ec74a79959c47b24cc517693ffeb1931a69c51f9
Gregory Wagner <wagner.greg@gmail.com>
2020-11-16 22:27:36 -0500

    Updates

--- examples/forced_bickley.jl
+++ examples/forced_bickley.jl
@@ -1,5 +1,6 @@
 using JLD2
 using Plots
+using Printf
 
 using Oceananigans
 using Oceananigans.Advection
@@ -19,41 +20,43 @@ U(y) = sech(y)
 relax_u(x, y, z, t, u, τ) = 1/τ * (U(y) - u)
 relax_c(x, y, z, t, c, τ) = 1/τ * (C(y) - c)
 
-force_u = Forcing(relax_u, field_dependencies=:u, parameters=1e-2)
-force_c = Forcing(relax_c, field_dependencies=:c, parameters=1e-2)
+force_u = Forcing(relax_u, field_dependencies=:u, parameters=1e2)
+force_c = Forcing(relax_c, field_dependencies=:c, parameters=1e2)
 
 model = IncompressibleModel(timestepper = :RungeKutta3, 
                               advection = WENO5(),
                                    grid = grid,
                                buoyancy = nothing,
                                 tracers = :c,
-                                closure = IsotropicDiffusivity(ν=1e-5),
-                                forcing = (u=force_u, c=force_c)
-                           )
+                                closure = IsotropicDiffusivity(ν=1e-4, κ=1e-4),
+                                forcing = (u=force_u, c=force_c))
 
-# ## Initial conditions
-#
-# Our initial condition randomizes `model.velocities.u` and `model.velocities.v`.
-# We ensure that both have zero mean for aesthetic reasons.
+# Initial conditions
+
+ϵ = 0.01
 
 set!(model,
-     u = (x, y, z) -> sech(y) - 0.1 * sech(y) * sin(x) * (tanh(y) * sin(y) - cos(y)),
-     v = (x, y, z) -> 0.1 * sech(y) * cos(x) * sin(y),
-     c = (x, y, z) -> sin(y),
-)
+     u = (x, y, z) -> sech(y)^2 * (1 + ϵ * randn()),
+     v = (x, y, z) -> ϵ * sech(y)^2 * randn(),
+     c = (x, y, z) -> sin(y / 4))
 
 u, v, w = model.velocities
 
 ω = ComputedField(∂x(v) - ∂y(u))
 
-progress(sim) = @info "Iteration: $(sim.model.clock.iteration), time: $(round(Int, sim.model.clock.time))"
+progress(sim) = @info(@sprintf("Iter: %d, time: %.1f, max|u|: %.2f",
+                               sim.model.clock.iteration, sim.model.clock.time, maximum(abs, interior(u))))
 
-simulation = Simulation(model, Δt=0.2, stop_time=200, iteration_interval=100, progress=progress)
+wizard = TimeStepWizard(cfl=1.0, Δt=1e-1, max_change=1.1)
 
-simulation.output_writers[:fields] = JLD2OutputWriter(model, merge(model.velocities, (ω=ω,)),
-                                                      schedule = TimeInterval(2),
+simulation = Simulation(model, Δt=wizard, stop_time=100, iteration_interval=100, progress=progress)
+
+simulation.output_writers[:fields] = JLD2OutputWriter(model, merge(model.velocities, model.tracers, (ω=ω,)),
+                                                      schedule = TimeInterval(1.0),
                                                       prefix = "forced_bickley",
                                                       force = true)
+
+@info "Running a simulation of a forced Bickley jet..."
 
 run!(simulation)
 
@@ -61,11 +64,12 @@ file = jldopen(simulation.output_writers[:fields].filepath)
 
 iterations = parse.(Int, keys(file["timeseries/t"]))
 
-xu, yu, zu = nodes(model.velocities.u)
-xv, yv, zv = nodes(model.velocities.v)
+xu, yu, zu = nodes(u)
+xv, yv, zv = nodes(v)
 xω, yω, zω = nodes(ω)
+xc, yc, zc = nodes(model.tracers.c)
 
-@info "Making a neat movie of vorticity and speed..."
+@info "Making a neat movie..."
 
 anim = @animate for (i, iteration) in enumerate(iterations)
 
@@ -77,12 +81,16 @@ anim = @animate for (i, iteration) in enumerate(iterations)
     t = file["timeseries/t/$iteration"]
     ω = file["timeseries/ω/$iteration"][:, :, 1]
     u = file["timeseries/u/$iteration"][:, :, 1]
+    c = file["timeseries/c/$iteration"][:, :, 1]
 
-    ωlim = 2.0
+    ωlim = 1
     ωlevels = range(-ωlim, stop=ωlim, length=20)
 
-    ulim = 1.0
+    ulim = 1
     ulevels = range(-ulim, stop=ulim, length=20)
+
+    clim = 1
+    clevels = range(-clim, stop=clim, length=20)
 
     kwargs = (xlabel="x", ylabel="y", aspectratio=1, linewidth=0, colorbar=true,
               xlims=(-model.grid.Lx/2, model.grid.Lx/2), ylims=(-model.grid.Ly/2, model.grid.Ly/2))
@@ -91,6 +99,12 @@ anim = @animate for (i, iteration) in enumerate(iterations)
                        color = :balance,
                       levels = ulevels,
                        clims = (-ulim, ulim),
+                      kwargs...)
+
+    c_plot = contourf(xc, yc, clamp.(c, -clim, clim)';
+                       color = :balance,
+                      levels = clevels,
+                       clims = (-clim, clim),
                       kwargs...)
              
     ω_plot = contourf(xω, yω, clamp.(ω, -ωlim, ωlim)';
@@ -101,8 +115,9 @@ anim = @animate for (i, iteration) in enumerate(iterations)
 
     u_title = @sprintf("u at t = %.1f", t)
     ω_title = @sprintf("ω at t = %.1f", t)
+    c_title = @sprintf("c at t = %.1f", t)
 
-    plot(u_plot, ω_plot, title=[u_title ω_title], layout=(1, 2), size=(1200, 500))
+    plot(u_plot, ω_plot, c_plot, title=[u_title ω_title c_title], layout=(1, 3), size=(1500, 400))
 end
 
 gif(anim, "forced_bickley.gif", fps = 8) # hide
