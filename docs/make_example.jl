bd0e096017caaa0116b75d39b75ba48d33a46243
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-12-03 22:53:07 -0500

    Some success making one example

--- docs/make_example.jl
+++ docs/make_example.jl
@@ -1,35 +1,29 @@
-push!(LOAD_PATH, "..")
-
 using Documenter
-using DocumenterCitations
 using Literate
 using Plots  # to avoid capturing precompilation output by Literate
 using Oceananigans
 
-#####
-##### Build example
-#####
+formatted_name = Dict(
+    "one_dimensional_diffusion" => "One-dimensional diffusion"
+)
 
 # Gotta set this environment variable when using the GR run-time on Travis CI.
 # This happens as examples will use Plots.jl to make plots and movies.
 # See: https://github.com/jheinen/GR.jl/issues/278
 ENV["GKSwstype"] = "100"
 
-bib_filepath = joinpath(dirname(@__FILE__), "oceananigans.bib")
-bib = CitationBibliography(bib_filepath)
-
 const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
-const OUTPUT_DIR   = joinpath(@__DIR__, "src", "generated")
+const OUTPUT_DIR   = joinpath(@__DIR__, "examples")
 
 example = ARGS[1]
-example_filepath = joinpath(EXAMPLES_DIR, example * ".jl")
+example_filepath = joinpath(EXAMPLES_DIR, "$example.jl")
 
 @info "Making example Literate: $example_filepath"
 Literate.markdown(example_filepath, OUTPUT_DIR, documenter=true)
 
 @info "Building docs..."
-makedocs(bib,
-	 sitename = "Oceananigans.jl",
-          authors = "Ali Ramadhan, Gregory Wagner, John Marshall, Jean-Michel Campin, Chris Hill",
-            pages = [example => "generated/$example.md"]
+makedocs(
+    sitename = "Oceananigans.jl",
+    source = "examples",
+    pages = [formatted_name[example] => "$example.md"]
 )
