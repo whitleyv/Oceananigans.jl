6cd24cdba47cd00946e0dfb3760d5551f94572e6
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-01-07 06:02:32 -0500

    Better setup and animation

--- examples/slanted_plume.jl
+++ examples/slanted_plume.jl
@@ -21,8 +21,7 @@ topology=(Bounded, Bounded, Bounded)
 grid = RegularCartesianGrid(topology=topology, size=(1, N, N), x=(0, 1), y=(0, L), z=(-L, 0))
 
 # Ice shelf with constant slope
-θ_ice_shelf = 45.0
-inside_ice_shelf(x, y, z) = z >= -L + y*sind(θ_ice_shelf)
+inside_ice_shelf(x, y, z) = z >= -L + y
 
 localized_heat_flux(x, y, t) = 0.2L <= y <= 0.3L ? -1e-4 : 0.0
 T_top_bc = FluxBoundaryCondition(localized_heat_flux)
@@ -33,16 +32,14 @@ T_bcs = TracerBoundaryConditions(grid, bottom=T_top_bc)
 model = IncompressibleModel(
                    grid = grid,
             timestepper = :RungeKutta3,
-              advection = UpwindBiasedFifthOrder(),
+              advection = WENO5(),
                 closure = IsotropicDiffusivity(ν=1e-3, κ=1e-3),
     boundary_conditions = (T=T_bcs,),
       immersed_boundary = inside_ice_shelf
 )
 
-# initial condition
-# setting velocitiy to zero inside the cylinder and 1 everywhere else
-T₀(x, y, z) = 0.3L <= y <= 0.4L && -0.95L <= z <= -0.85L ? 20.0 : 19.0
-# T₀(x, y, z) = inside_ice_shelf(x, y, z) ? 20.0 : 20 # + 0.001z
+# Square blob warm anomaly
+T₀(x, y, z) = 0.25L <= y <= 0.4L && -0.95L <= z <= -0.8L ? 20.0 : 19.0
 set!(model, T=T₀)
 
 progress(sim) = @info @sprintf("Iteration: % 4d, time: %.2f, extrema(w) = (%.4e, %.4e), extrema(T) = (%.4e, %.4e)",
@@ -73,30 +70,27 @@ T, S = model.tracers
 
 xc, yc, zc = nodes(T)
 
+mask = inside_ice_shelf.(nodes(T, reshape=true)...)[1, :, :]
+
 anim = @animate for (i, iteration) in enumerate(iterations)
     @info "Plotting frame $i from iteration $iteration..."
 
     t = file["timeseries/t/$iteration"]
     T_slice = file["timeseries/T/$iteration"][1, :, :]
+    T_slice[mask] .= NaN
 
-    T_min, T_max = extrema(T_slice)
-    T_levels = vcat([T_min], range(T_min, T_max, length=50), [T_max])
-
-    v_plot = heatmap(yc, zc, T_slice';
+    T_plot = heatmap(yc, zc, T_slice';
                       linewidth = 0,
                           color = :thermal,
                     aspectratio = 1,
                           title = @sprintf("T(x, y, t = %s)", prettytime(t)),
-                         xlabel = "x",
-                         ylabel = "y",
-                        #  levels = T_levels,
-                          xlims = (grid.yF[1], grid.yF[grid.Ny]),
-                          ylims = (grid.zF[1], grid.zF[grid.Nz]),
-                          clims = (19, 20)
+                         xlabel = "y",
+                         ylabel = "z",
+                          xlims = (grid.yF[1], grid.yF[grid.Ny+1]),
+                          ylims = (grid.zF[1], grid.zF[grid.Nz+1]),
+                          clims = (19, 20),
+                            dpi = 200
     )
-
-    # plot!(circleShape(30,20,1),seriestype=[:shape,],linecolor=:black,
-    #       legend=false,fillalpha=0, aspect_ratio=1)
 end
 
 gif(anim, "slanted_plume.gif", fps=15)
